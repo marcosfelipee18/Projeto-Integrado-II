fdf082784d20c61797629a11a7c9208082add1d5
Marcos Felipe <98355126+marcosfelipee18@users.noreply.github.com>
2023-11-03 16:26:54 -0300

    Add project files.

---
+++ Faculdade/Faculdade.cpp
@@ -0,0 +1,38 @@
+// Faculdade.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	char nome[50];
+	char endereco[100];
+	char telefone[20];
+
+	printf("Digite seu nome: ");
+	gets_s(nome, sizeof(nome));
+
+	printf("Digite seu endereco: ");
+	gets_s(endereco, sizeof(endereco));
+
+	printf("Digite seu telefone: ");
+	gets_s(telefone, sizeof(telefone));
+
+	printf("\n--- Dados inseridos ---\n");
+	printf("Nome: %s\n", nome);
+	printf("Endereco: %s\n", endereco);
+	printf("Telefone: %s\n", telefone);
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
